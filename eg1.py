2601855352b418f22588025f982706fce507b8cb
anurag <anuragsharma778@gmail.com>
2021-01-17 19:34:02 +0530

    initial commit

---
+++ eg1.py
@@ -0,0 +1,27 @@
+'''
+# Sample code to perform I/O:
+
+name = input()                  # Reading input from STDIN
+print('Hi, %s.' % name)         # Writing output to STDOUT
+
+# Warning: Printing unwanted or ill-formatted data to output will cause the test cases to fail
+'''
+
+# Write your code here
+t=int(raw_input())
+for i in range(t):
+    n=int(raw_input())
+    a=list(map(int,raw_input().split()))[:n]
+    k=a
+    print k
+    a.sort()
+    print a
+    min=1000
+    for i in range(n):
+        for j in range(i,n):
+            if i!=j:
+                k=(a[i] and a[j])^(a[i] or a[j])
+                if k<min:
+                    print "Comparing ",a[i],'and ',a[j],' value = ',k
+                    min=k
+    print(min)
